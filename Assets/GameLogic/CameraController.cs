dcaa7fd3ab18382a3721e3627d038478e01bd440
Henrik Hinrichs <henrik.hinrichs@framefield.com>
2019-11-09 19:17:52 +0100

    Move camera to any position

--- Assets/GameLogic/CameraController.cs
+++ Assets/GameLogic/CameraController.cs
@@ -14,18 +14,17 @@ namespace GameLogic
         private float _smoothTime = 1.0f;
 
         [SerializeField]
-        private float _offset = 3f;
+        private Vector2 _offset = new Vector2(0f, 3f);
 
         private void Update()
         {
             var cameraPosition = _camera.transform.position;
-            var targetCameraPosition = _catStacker.Top.y + _offset;
-            var damped = Mathf.SmoothDamp(cameraPosition.y, targetCameraPosition, ref _cameraVelocity, _smoothTime);
-
-            cameraPosition.y = damped;
-            _camera.transform.position = cameraPosition;
+            var targetCameraPosition = _catStacker.Top + _offset;
+            var damped = Vector3.SmoothDamp(cameraPosition, targetCameraPosition, ref _cameraVelocity, _smoothTime);
+            _camera.transform.position = new Vector3(damped.x, damped.y, cameraPosition.z);
+            ;
         }
 
-        private float _cameraVelocity = 0f;
+        private Vector3 _cameraVelocity = Vector2.zero;
     }
 }
